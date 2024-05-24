354b92ae1977ed1e5754c0f0404a108d257b5e0d
trainthunder <kitikun.po@gmail.com>
2024-05-24 16:02:28 +0700

    fix: change answer ex-6.js

--- ex-6.js
+++ ex-6.js
@@ -1,9 +1,12 @@
 let height = undefined;
 let result = function (height) {
+  return height ?? `Height is not defined`;
+  /*
   if (height === undefined) {
     return `Height is not defined`;
   } else {
     return `Hell Yeah!!`;
   }
+  */
 };
 console.log(result()); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
